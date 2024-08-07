89a4b8e0a44d6b0be872876162eb46287174ed74
FAZEL UNITY <53996181+fazelunity0054@users.noreply.github.com>
2024-08-07 17:33:06 +0330

    Finalize #2

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,26 +2,32 @@ import {Client} from "discord.js";
 import CustomTelegraf from "./telegraf/CustomTelegraf";
 import {unity_hotreload} from "./index";
 
+declare global {
+	var discordBot: Client | undefined;
+	var telegramBot: CustomTelegraf | undefined;
+}
 
-
-export let telegramBot = stableTelegram();
+export let telegramBot = global.telegramBot ?? stableTelegram();
+global.telegramBot = telegramBot;
 
 function stableTelegram() {
 	const T = new CustomTelegraf('TD',process.env.TELEGRAM+"");
 	T.onDisconnect(async ()=>{
 		console.log("Reconnecting telegram...");
 		telegramBot = stableTelegram();
+		global.telegramBot = telegramBot;
 		await T.waitToReady();
 	})
 	return T;
 }
 
-export let discordBot = new Client({
+export let discordBot = global.discordBot ?? new Client({
 	intents: ['MessageContent','GuildMessages',"Guilds","GuildMembers"]
 });
+global.discordBot = discordBot;
 export const BotsReadyPromise = Promise.all([
-	telegramBot.waitToReady(),
-	discordBot.login(process.env.DISCORD)
+	telegramBot.waitToReady().then(r => r.username),
+	discordBot.login(process.env.DISCORD).then(()=>discordBot.user?.username)
 ]).then((e)=>{
 	console.log("All Bots Ready!",...e)
 })
